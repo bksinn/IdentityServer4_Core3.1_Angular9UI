84cba386d233a6e1c39e44cdd460fa716787c484
Bharat Singh <bksingh@g.cofc.edu>
2020-04-17 12:45:35 -0400

    Adding sample account controller--not from QuickStart

--- SampleCoreAng/Quickstart/Extensions.cs
+++ SampleCoreAng/Quickstart/Extensions.cs
@@ -25,5 +25,23 @@ namespace IdentityServer4.Quickstart.UI
             
             return controller.View(viewName, new RedirectViewModel { RedirectUrl = redirectUri });
         }
+
+        /// <summary>
+        /// Determines whether the client is configured to use PKCE.
+        /// </summary>
+        /// <param name="store">The store.</param>
+        /// <param name="client_id">The client identifier.</param>
+        /// <returns></returns>
+        public static async Task<bool> IsPkceClientAsync(this IClientStore store, string client_id)
+        {
+            if (!string.IsNullOrWhiteSpace(client_id))
+            {
+                var client = await store.FindEnabledClientByIdAsync(client_id);
+                return client?.RequirePkce == true;
+            }
+
+            return false;
+        }
     }
+}
 }
